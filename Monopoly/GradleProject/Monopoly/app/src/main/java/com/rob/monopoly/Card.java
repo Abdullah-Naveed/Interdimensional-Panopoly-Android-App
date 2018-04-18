b22b7807c00c2443d2dcf12a5074096b3ba270bc
eriksburka <36045335+eriksburka@users.noreply.github.com>
2018-04-18 17:27:12 +0100

    CHANCE CARD FUNCTIONS FINISHED

--- Monopoly/GradleProject/Monopoly/app/src/main/java/com/rob/monopoly/Card.java
+++ Monopoly/GradleProject/Monopoly/app/src/main/java/com/rob/monopoly/Card.java
@@ -12,7 +12,6 @@ public class Card{
         this.message=message;
     }
 
-
     public void runAway(){
 
         Random randomNumber = new Random();
@@ -40,6 +39,18 @@ public class Card{
         GameState.getInstance().getCurrentPlayer().deposit(i*50);
 
         // It's your birthday you get 50 euro from every player.
+    }
+    
+    public void christmas(){
+    	int i = GameState.getInstance().getNumPlayers();
+    	GameState.getInstance().getCurrentPlayer().withdraw(i*20);
+    	// It's christmas so you buy a present for all the players
+    }
+    
+    public void christmas2(){
+    	int i = GameState.getInstance().getNumPlayers();
+    	GameState.getInstance().getCurrentPlayer().deposit(i*20);
+    	// It's christmas you get a present from all of the players
     }
 
     public void meeting(){
@@ -97,8 +108,137 @@ public class Card{
 
         // the NEGATIVE villain made damages to all of your properties. pay 25 for every property you own
     }
+    
+    public void coffee(){
+    	GameState.getInstance().getCurrentPlayer().withdraw(25);
+    	// hero picked you up in his vehicle. yous go for a coffee which cost you 25
+    }
+    
+    public void fan(){
+    	GameState.getInstance().getCurrentPlayer().withdraw(15);
+    	// You see your favourite hero on the streets, he charges you 15 for a picture
+    }
+    
+    public void underPaid(){
+    	int i = GameState.getInstance().getCurrentPlayer().getNumProperties();
+    	GameState.getInstance().getCurrentPlayer().withdraw(i*15);
+    	
+    	// Villain rats you out for underpaying for your properties. you must pay an extra 15 for every property owned
+    
+    }
+    
+    public void taxFraud(){
+    	int i = GameState.getInstance().getCurrentPlayer().getBalance();
+    	i = i/15;
+    	
+    	GameState.getInstance().getCurrentPlayer().withdraw(i);
+    	
+    	// you're caught for not paying the right amount of tax.
+    	
+    }
+    
+//    public void droppingMoney(){
+//    	GameState.getInstance().getNextPlayer().withdraw(50);
+//    	GameState.getInstance().getCurrentPlayer().deposit(50);
+//    	
+//    	// the player infront of you dropped 50 euro. you and HERO race for it but you get it first.
+//    }
+    
+    
+   public void taxMan(){
+	   int i = GameState.getInstance().getCurrentPlayer().getBalance();
+	   i = i/10;
+	   GameState.getInstance().getCurrentPlayer().withdraw(i);
+	   
+	  // Villain comes to your house disguised as a taxman and scammed you.
+   }
+   
+   public void interest(){
+	   int i = GameState.getInstance().getCurrentPlayer().getBalance();
+	   i = i/16;
+	   GameState.getInstance().getCurrentPlayer().deposit(i);
+	  
+	   // you gained interest on your savings from the bank
+   }
+   
+   public void moneyBack(){
+	   GameState.getInstance().getCurrentPlayer().deposit(50);
+	   // Hero gave you a little present for being a loyal fan
+   }
+   
+   public void jail(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(34);
+	   // You hate villain because he is NEGATIVE, NEGATIVE, NEGATIVE. you get sent to jail for assaulting him.
+	   
+   }
+   
+   public void jail2(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(34);
+	   // you are caught stealing Villains vehicle. you get caught and sent to jail
+   }
+   
+   public void game(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(5);
+	   
+	   // it's been a long weekend so you and HERO go to play a game
+   }
+   
+   public void game2(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(29);
+	   
+	   // it's been a long weekend so you and Villain go to play a game
+   }
+   
+   public void freeParking(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(20);
+	   
+	   // You manage to find free parking before you meet up with HERO.
+   }
+   
+   public void teleporting(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(16);
+	   
+	   // HERO bring you in his vehicle to a secret location. You's find a teleporting device and decide to try it out
+   }
+   
+   public void houseParty(){
+	   int i = GameState.getInstance().getCurrentPlayer().getNumProperties();
+	   GameState.getInstance().getCurrentPlayer().withdraw(i*25);
+	   
+	   // You host a house party in every property you own that week inviting people such as HERO,HERO,HERO,HERO. Some damages were made to all properties.
+   }
+   
+   public void negativeSocialMedia(){
+	   Random randomNumber = new Random();
+       int i = randomNumber.nextInt(15)+1;
 
+       GameState.getInstance().getCurrentPlayer().move(i);
+	   // you receive a lot of backlash for saying that HERO is NEGATIVE, NEGATIVE and NEGATIVE online and are chased on the streets until you find yourself somewhere safe.
+   }
+   
+   public void wallet(){
+	   GameState.getInstance().getCurrentPlayer().deposit(130);
+	   // you see a villain doing his favourite activity. You manage to rob his wallet whilst he wasn't looking
+   }
+   
+   public void smashedPhone(){
+	   GameState.getInstance().getCurrentPlayer().withdraw(150);
+	   // Villain smashes your new phone with this weapon of choice. You have to pay 150 to repair it
+   }
+   
+   public void goToInsurance(){
+	   GameState.getInstance().getCurrentPlayer().setPlayerLocation(11);
+	   
+	   // Villain spread rumours you're a tax fraud so you head to the tax office.
+   }
+   
+  public void pawnShop(){
+	  GameState.getInstance().getCurrentPlayer().withdraw(80);
+	  
+	  // you go to a pawn shop and end up seeing HEROS CLOTHING. you end up buying it.
+  }
 
+   
     // Possible ideas for chance card //
 
     // - steal someones property.
