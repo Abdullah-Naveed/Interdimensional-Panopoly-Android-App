8caab24ee9eda66456b411769ded23d81b2ed876
RobertMetcalfe10 <robert.metcalfe@ucdconnect.ie>
2018-05-01 15:51:27 +0100

    started testing

--- Monopoly/GradleProject/Monopoly/app/src/androidTest/java/com/rob/monopoly/ExampleInstrumentedTest.java
+++ Monopoly/GradleProject/Monopoly/app/src/androidTest/java/com/rob/monopoly/ExampleInstrumentedTest.java
@@ -16,6 +16,9 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    GameState gameState=GameState.getInstance();
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
--- Monopoly/GradleProject/Monopoly/app/src/test/java/com/rob/monopoly/ExampleUnitTest.java
+++ Monopoly/GradleProject/Monopoly/app/src/test/java/com/rob/monopoly/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.rob.monopoly;
 
+import android.view.ViewGroup;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +13,28 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+
+    Bank bank;
+
+    @Before
+    public void setUp()
+    {
+        bank=new Bank(1000);
     }
+
+    @Test
+    public void deposit() throws Exception {
+        bank.deposit(100);
+        assertEquals(bank.getBalance(),1100);
+    }
+    @Test
+    public void withdraw() throws Exception {
+        bank.withdraw(100);
+        assertEquals(bank.getBalance(),900);
+    }
+    @Test
+    public void getBalance() throws Exception {
+        assertEquals(bank.getBalance(),1000);
+    }
+
 }
